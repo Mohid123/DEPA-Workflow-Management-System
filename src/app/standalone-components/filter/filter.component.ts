20e9c5c4160de3662ee1479f1ac7bc26bc098e6c
Mohid123 <muhammadmohid141@gmail.com>
2023-04-18 09:36:24 +0500

    filter component design logic and new features

--- src/app/standalone-components/filter/filter.component.ts
+++ src/app/standalone-components/filter/filter.component.ts
@@ -1,30 +1,36 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TuiButtonModule, TuiHostedDropdownModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiHintModule, TuiHostedDropdownModule } from '@taiga-ui/core';
 import { TuiDataListWrapperModule, TuiInputModule } from '@taiga-ui/kit';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'filter',
   standalone: true,
-  imports: [CommonModule, TuiHostedDropdownModule, TuiButtonModule, TuiDataListWrapperModule, TuiInputModule, SearchBarComponent],
+  imports: [CommonModule, TuiHostedDropdownModule, TuiButtonModule, TuiDataListWrapperModule, TuiInputModule, SearchBarComponent, TuiHintModule],
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilterComponent {
   open = false;
-  readonly items = ['Sort by Acsending', 'Sort by Decsending', 'Sort by Latest', 'Sort by Oldest'];
+  @Input() items: any[];
   @Input() searchEnabled: boolean = true;
   @Input() showSortCategories: boolean = true;
   @Input() filterBy: string;
-  itemIsActive: boolean = false;
+  @Output() applyStatus = new EventEmitter();
+  @Output() resetFilters = new EventEmitter();
+  isFilterApplied = false;
 
   searchTableData(searchStr: string) {
     console.log(searchStr)
   }
 
-  setActiveAndFilter() {
-    this.itemIsActive = !this.itemIsActive;
+  setFilterAndApplyActive(value: any) {
+    this.applyStatus.emit(value);
+    if(this.items?.some(val => val.name === value.applyOn)) {
+      this.items[value.index].status = 'active';
+      this.isFilterApplied = true;
+    }
   }
 }
--- src/app/standalone-components/table-view/table-view.component.ts
+++ src/app/standalone-components/table-view/table-view.component.ts
@@ -1,16 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RouterModule } from '@angular/router';
 import { FilterComponent } from '../filter/filter.component';
+import { TuiPaginationModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-table-view',
   standalone: true,
-  imports: [CommonModule, SearchBarComponent, RouterModule, FilterComponent],
+  imports: [CommonModule, SearchBarComponent, RouterModule, FilterComponent, TuiPaginationModule],
   templateUrl: './table-view.component.html',
   styleUrls: ['./table-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableViewComponent {
+  @Input() tableColumns: string[] = ['Company Name', 'Submodule Name', 'Module Name', 'Status'];
+  filterMenu =  [
+    {name: 'Sort by Acsending', status: 'idle', icon: 'fa fa-sort-alpha-asc fa-lg'},
+    {name: 'Sort by Decsending', status: 'idle', icon: 'fa fa-sort-alpha-desc fa-lg'},
+    {name: 'Sort by Latest', status: 'idle', icon: 'fa fa-calendar-check-o fa-lg'},
+    {name: 'Sort by Oldest', status: 'idle', icon: 'fa fa-calendar-times-o fa-lg'}
+  ];
+  statusMenu = [
+    {name: 'Draft', status: 'idle', icon: ''},
+    {name: 'Published', status: 'idle', icon: ''}
+  ];
+  length = 20;
+  index = 1;
+ 
+  goToPage(index: number): void {
+    this.index = index;
+    console.info('New page:', index);
+  }
+
+  sendFilterValue(value: any) {
+    console.log(value);
+    //send api call here
+  }
 }
