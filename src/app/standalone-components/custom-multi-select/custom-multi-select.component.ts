1ce50d00122df7025904e26282813bb94fb8e250
Mohid123 <muhammadmohid141@gmail.com>
2023-04-19 10:11:18 +0500

    custom multiselect complete

--- src/app/standalone-components/custom-multi-select/custom-multi-select.component.ts
+++ src/app/standalone-components/custom-multi-select/custom-multi-select.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, OnDestroy, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiCheckboxLabeledModule, TuiInputModule } from '@taiga-ui/kit';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TuiTextfieldControllerModule } from '@taiga-ui/core';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
  
 
 @Component({
@@ -13,14 +14,16 @@ import { TuiTextfieldControllerModule } from '@taiga-ui/core';
   styleUrls: ['./custom-multi-select.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CustomMultiSelectComponent {
+export class CustomMultiSelectComponent implements OnDestroy {
   open = false;
   searchValue = new FormControl();
+  destroy$ = new Subject();
+  inputFieldArr: string[] = [];
   @ViewChild('customElem') customElem?: ElementRef;
   @Input() items: any[] = [
-    {name: 'Ali', control: new FormControl(false)},
-    {name: 'Abid', control: new FormControl(false)},
-    {name: 'Junaid', control: new FormControl(false)},
+    {name: 'Ali khan raja raunaqzai', control: new FormControl(false)},
+    {name: 'Abid ahmad tarakai', control: new FormControl(false)},
+    {name: 'Junaid mehmood', control: new FormControl(false)},
     {name: 'Fadi', control: new FormControl(false)},
     {name: 'Ahtasham', control: new FormControl(false)},
     {name: 'Sadiq', control: new FormControl(false)}
@@ -33,7 +36,32 @@ export class CustomMultiSelectComponent {
     }
   }
 
+  @HostListener('document:scroll', ['$event'])
+  listenToDOMScroll() {
+    if(!this.customElem.nativeElement.contains(event.target)) {
+      this.open = false;
+    }
+  }
+
   toggleDropDown() {
     this.open = !this.open;
+  }
+
+  selectValueAndPushToInput(index: number, event: any) {
+    if(event.target?.checked === true) {
+      this.inputFieldArr.push(this.items[index]?.name)
+    }
+    else {
+      this.inputFieldArr.splice(index, 1);
+    }
+  }
+
+  removeItem(index: number) {
+    this.inputFieldArr.splice(index, 1);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
   }
 }
