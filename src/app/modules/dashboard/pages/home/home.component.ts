3128757590a5984845b76355f9487c1a06f7616f
Mohid123 <muhammadmohid141@gmail.com>
2023-04-27 14:49:28 +0500

    pagination on module list page

--- src/app/modules/dashboard/pages/home/home.component.ts
+++ src/app/modules/dashboard/pages/home/home.component.ts
@@ -8,6 +8,14 @@ export class HomeComponent {
   Arr = Array;
   num: number = 7;
 
+  length = 20;
+  index = 0;
+
+  goToPage(index: number): void {
+    this.index = index;
+    console.info('New page:', index);
+  }
+
   constructor() {
   }
 
