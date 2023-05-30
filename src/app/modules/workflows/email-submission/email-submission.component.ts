81ad87ed8e9492e27bb6e4879af67e2490083f0f
Mohid123 <muhammadmohid141@gmail.com>
2023-05-30 16:15:55 +0500

    handled api call on email submission page

--- src/app/modules/workflows/email-submission/email-submission.component.ts
+++ src/app/modules/workflows/email-submission/email-submission.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { WorkflowsService } from '../workflows.service';
 import { NotificationsService } from 'src/core/core-services/notifications.service';
 import { TuiNotification } from '@taiga-ui/core';
@@ -11,26 +11,26 @@ import { TuiNotification } from '@taiga-ui/core';
 })
 export class EmailSubmissionComponent implements OnDestroy {
   destory$ = new Subject();
-  savingDecision = new Subject<boolean>();
+  savingDecision = new BehaviorSubject<boolean>(true);
+  userDecision: any;
 
   constructor(private activatedRoute: ActivatedRoute, private workflowService: WorkflowsService, private notif: NotificationsService) {
     this.activatedRoute.queryParams
     .pipe(takeUntil(this.destory$))
     .subscribe(res => {
-      debugger
-      this.savingDecision.next(true)
       if(res) {
         const payload: any = {
           stepId: res['stepId'],
           userId: res['userId'],
-          remarks: res['remarks'],
           isApproved: res['isApproved']
         }
-        console.log(res)
-        // this.workflowService.updateSubmissionWorkflow(res['id'], payload).pipe(takeUntil(this.destory$))
-        // .subscribe((res: any) => {
-        //   this.savingDecision.next(false);
-        // })
+        this.userDecision = payload.isApproved
+        this.savingDecision.next(true)
+        this.workflowService.updateSubmissionWorkflow(res['submissionId'], payload).pipe(takeUntil(this.destory$))
+        .subscribe((res: any) => {
+          console.log(res)
+          this.savingDecision.next(false);
+        })
       }
       else {
         this.savingDecision.next(false)
