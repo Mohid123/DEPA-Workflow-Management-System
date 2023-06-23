5cb4feecb2f6252d5f429f1862d52736109c730f
Mohid123 <muhammadmohid141@gmail.com>
2023-06-23 13:03:04 +0500

    path redirect on empty url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,11 @@ const routes: Routes = [
     loadChildren: () => import('./modules/forms/forms.module').then(m => m.FormsModule)
   },
   {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  {
     path: '**',
     component: NotFoundComponent
   }
