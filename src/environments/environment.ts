71f098e8a363e0ce42da5ed974a1d62f85af01f2
Mohid123 <muhammadmohid141@gmail.com>
2023-08-30 09:35:55 +0500

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   production: false,
   serviceWorker: false,
   appVersion: 'v1.0.00',
-  apiUrl: 'http://quaidtech.ddns.net:8500/v1',
+  apiUrl: 'http://localhost:3000/v1',
   appThemeName: 'Depa Frontend'
 };
