3ce492afde8990b97c239fa4d047386d931f6d83
Mohid123 <muhammadmohid141@gmail.com>
2023-10-25 17:37:45 +0500

    send debugger for tesing

--- src/app/modules/workflows/add-submission/add-submission.component.ts
+++ src/app/modules/workflows/add-submission/add-submission.component.ts
@@ -396,6 +396,7 @@ export class AddSubmissionComponent implements OnDestroy, OnInit {
         this.hooks.forEach(val => {
           if(val?.name == 'beforeSubmit') {
             val.code = new Function('return ' + val.code)();
+            debugger
             val.code(submission?.data, 'acb-01', 'sf-03', this.formService, takeUntil, this.destroy$, forkJoin);
           }
         })
